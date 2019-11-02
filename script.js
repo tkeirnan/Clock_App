7c4942e82f79d1f690899fa51fc202c8951f17d2
Tommy Keirnan <tkeirnan@gmail.com>
2019-11-02 14:13:52 -0400

    corrected javascript, html, and css code.

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-var d = new Date();
+// IIFE
 
-let year = d.getFullYear();
-let month = d.getMonth();
-let day = d.getDay();
-let date = d.getDate();
-let hours = d.getHours();
-let minutes = d.getMinutes();
-let seconds = d.getSeconds();
+(function() {
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
+    const dayArray = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',]; 
 
-//console.log(`${day} - ${month} ${date}`);
+    function updateClock() {
+        const today = new Date();
 
-document.getElementById('year').innerHTML= year;
+        const day = today.getDay();
+        const dayNum = today.getDate();
+        const month = today.getMonth();
+        
+        const currentDateEl = document.getElementById('day-of-week');
+        currentDateEl.innerText = dayArray[day] + ', ' + monthArray[month] + ' ' + dayNum;
+
+        const currentHourEl = document.getElementById('hours'); 
+        currentHourEl.innerText = formatHours(today.getHours());
+        
+        const currentMinEl = document.getElementById('minutes');
+        currentMinEl.innerText = formatMinutes(today.getMinutes());
+
+        const currentSecEl = document.getElementById('seconds');
+        currentSecEl.innerText = formatSeconds(today.getSeconds());
+
+        function formatHours(hours){
+            if (hours < 10) {
+                return '0' + hours;
+            }
+            return hours;
+        }
+        
+        function formatMinutes(minutes){
+            if (minutes < 10) {
+                return '0' + minutes;
+            }
+            return minutes;
+        }
+        
+        function formatSeconds(seconds){
+            if (seconds < 10) {
+                return '0' + seconds;
+            }
+            return seconds;
+        }
+
+    }
+ 
+    setInterval(updateClock, 1000);
+
+updateClock();
+
+})();
