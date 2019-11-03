394505cb99106c2c01c89eaac761d47a5debc2f8
Tommy Keirnan <tkeirnan@gmail.com>
2019-11-03 12:24:16 -0500

    added button to change time format from AmPm to military time.

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-// IIFE
+(function() {
+    const dayArray = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',]; 
+    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
+    
+    function updateCalendark() {
+            const today = new Date();
+    
+            const day = today.getDay();
+            const date = today.getDate();
+            const month = today.getMonth();
 
-(function() {
-    const dayArray = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday',]; 
-    const monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',];
-
-    function updateClock() {
-        const today = new Date();
-
-        const day = today.getDay();
-        const date = today.getDate();
-        const month = today.getMonth();
-
-        const currentHourEl = document.getElementById('hours'); 
-        currentHourEl.innerText = formatHours(today.getHours());
-        
-        const currentMinEl = document.getElementById('minutes');
-        currentMinEl.innerText = formatMinutes(today.getMinutes());
-
-        const currentSecEl = document.getElementById('seconds');
-        currentSecEl.innerText = formatSeconds(today.getSeconds());
-
-        const currentDateEl = document.getElementById('day-month-date');
-        currentDateEl.innerText = dayArray[day] + ', ' + monthArray[month] + ' ' + date;
-
-        function formatHours(hours){
-            if (hours < 10) {
-                return '0' + hours;
-            }
-            return hours;
-        }
-        
-        function formatMinutes(minutes){
-            if (minutes < 10) {
-                return '0' + minutes;
-            }
-            return minutes;
-        }
-        
-        function formatSeconds(seconds){
-            if (seconds < 10) {
-                return '0' + seconds;
-            }
-            return seconds;
-        }
-        
-        
-
+    const currentDateEl = document.getElementById('day-month-date');
+    currentDateEl.innerText = dayArray[day] + ', ' + monthArray[month] + ' ' + date;
     }
- 
 
-
-    setInterval(updateClock, 1000);
-
-updateClock();
-
+    setInterval(updateCalendark, 1000);
+    
+    updateCalendark();
+    
 })();
+
+
+var is24Hour = false,
+    timeDisplay = document.getElementById("clock"),
+    changeButton = document.getElementById("changeFormat");
+
+
+var updateTime = function() {
+    var currentDate = new Date(),
+        hours = currentDate.getHours();
+        hours = ("0" + hours).slice(-2);
+        minutes = currentDate.getMinutes(),
+        minutes = ("0" + minutes).slice(-2);
+        seconds = currentDate.getSeconds();
+        seconds = ("0" + seconds).slice(-2);
+
+    if(is24Hour) {
+        timeDisplay.innerText = hours + ":" + minutes + ":" + seconds;
+    }
+    else {
+        var suffix = hours >= 12 ? "PM" : "AM",
+            hours12 = hours % 12;
+        
+        timeDisplay.innerText = hours12 + ":" + minutes + ":" + seconds + " " + suffix;
+    }    
+
+};
+
+var changeFormat = function() {
+    is24Hour = !is24Hour;
+};
+
+changeButton.addEventListener("click", changeFormat);
+window.setInterval(updateTime, 1000);
+
+
+
