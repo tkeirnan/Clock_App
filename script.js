e0854108ed387e08a36205771cc66d55e10a3aa6
Tommy Keirnan <tkeirnan@gmail.com>
2019-11-19 23:32:03 -0500

    Final Commit

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 //IIFE -
-
-// (function() {
-//     let name = "Andy"; // can't access in the global scope
-// })();
-
-// console.log(name); //undefined
-
 (function() {
   const dayArray = [
     "Sunday",
@@ -37,47 +30,56 @@
   const changeButton = document.getElementById("changeFormat");
 
   function updateCalendar() {
-    console.log("updateCalendar() ran!");
-    const today = new Date();
+     const today = new Date();
 
-    const day = today.getDay();
-    const date = today.getDate();
-    const month = today.getMonth();
+     const day = today.getDay();
+     const date = today.getDate();
+     const month = today.getMonth();
 
-    const currentDateEl = document.getElementById("day-month-date");
-    currentDateEl.innerText =
-      dayArray[day] + ", " + monthArray[month] + " " + date;
+     const currentDateEl = document.getElementById("day-month-date");
+     currentDateEl.innerText =
+     dayArray[day] + ", " + monthArray[month] + " " + date;
   }
 
     var updateTime = function() {
-        console.log("updateTime() ran!");
+        console.log(is24Hour);
         var currentDate = new Date(),
           hours = currentDate.getHours();
-        hours = ("0" + hours).slice(-2);
-        (minutes = currentDate.getMinutes()), (minutes = ("0" + minutes).slice(-2));
-        seconds = currentDate.getSeconds();
-        seconds = ("0" + seconds).slice(-2);
+          hours = ("0" + hours).slice(-2);
+          (minutes = currentDate.getMinutes()), (minutes = ("0" + minutes).slice(-2));
+          seconds = currentDate.getSeconds();
+          seconds = ("0" + seconds).slice(-2);
 
-        if (is24Hour) {
-            timeDisplay.innerText = hours + ":" + minutes + ":" + seconds;
-        } else {
-          var suffix = hours >= 12 ? "PM" : "AM",
-            hours12 = hours % 12;
+          var suffix = hours >= 12 ? "PM" : "AM";
 
-        if (hours12 === 0) {
-            hours12 = 12;
+          if (is24Hour) {
+             displayTime(hours, suffix)
+            } else {
+              
+              let hours12 = hours % 12;
+
+            if (hours12 === 0) {
+                hours12 = 12;
+            }
+            hours12 = ("0" + hours).slice(-2);
+
+            displayTime(hours12, suffix)
         }
+        updateCalendar();   
+    };
 
+    function displayTime(hours, suffix){
         timeDisplay.innerText =
-            hours12 + ":" + minutes + ":" + seconds + " " + suffix;
-       }
-    updateCalendar();   
-  };
+                hours + ":" + minutes + ":" + seconds + " " + suffix;
 
-  var changeFormat = function() {
-    is24Hour = !is24Hour;
-  };
+    }
+
+    var changeFormat = function() {
+        is24Hour = !is24Hour;
+    };
 
   changeButton.addEventListener("click", changeFormat);
   window.setInterval(updateTime, 1000);
+
+  updateTime();
 })();
